e36d4b866ed98aec16b080bf7eef7a05de66880b
Aneesh Anil <aneesh@geektrust.in>
2024-06-07 23:01:00 +0530

    tests made clearer for board and cell ops

--- models/board_test.go
+++ models/board_test.go
@@ -11,8 +11,14 @@ func TestNewBoard(t *testing.T) {
 
 	assert.NotNil(t, b)
 	assert.Equal(t, 64, len(b.cells), "Board should have %d cells", 64)
-	assert.Equal(t, "A1", b.cells["A1"].Position)
-	assert.True(t, b.cells["A2"].neighbours[NORTH] != nil)
-	assert.True(t, b.cells["A1"].neighbours[WEST] == nil)
-	assert.True(t, b.cells["C3"].neighbours[SOUTH] == b.cells["C2"])
+}
+
+func TestBoard_CellAt_when_PositionIsValid_then_ReturnsCell(t *testing.T) {
+	b := NewBoard()
+	assert.NotNil(t, b.CellAt("A1"))
+}
+
+func TestBoard_CellAt_when_PositionIsInvalid_then_ReturnsNil(t *testing.T) {
+	b := NewBoard()
+	assert.Nil(t, b.CellAt("H15"))
 }
---
+++ models/cell_test.go
@@ -0,0 +1,17 @@
+package models
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCell_NeighbourAt(t *testing.T) {
+	b := NewBoard()
+	assert.Equal(t, "F6", b.CellAt("F5").NeighbourAt(NORTH).Position)
+}
+
+func TestCell_NeighbourAt_when_PositionIsInvalid_then_ReturnNil(t *testing.T) {
+	b := NewBoard()
+	assert.Nil(t, b.CellAt("H8").NeighbourAt(EAST))
+}
